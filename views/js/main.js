9d0a93be422b45e4a9cf3466a1380a23d088a7cb
Ivy Fu <ivy@hmsnz.com>
2017-04-19 16:51:09 +1200

    Reset moving pizza background, delete the unecessary animation

--- views/js/main.js
+++ views/js/main.js
@@ -499,8 +499,9 @@ function updatePositions() {
   window.performance.mark("mark_start_frame");
 
   var items = document.querySelectorAll('.mover');
+  var bodyScrollTop = document.body.scrollTop;
   for (var i = 0; i < items.length; i++) {
-    var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
+    var phase = Math.sin((bodyScrollTop / 1250) + (i % 5));
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
 
@@ -518,18 +519,18 @@ function updatePositions() {
 window.addEventListener('scroll', updatePositions);
 
 // 当页面加载时生成披萨滑窗
-document.addEventListener('DOMContentLoaded', function() {
-  var cols = 8;
-  var s = 256;
-  for (var i = 0; i < 200; i++) {
-    var elem = document.createElement('img');
-    elem.className = 'mover';
-    elem.src = "images/pizza.png";
-    elem.style.height = "100px";
-    elem.style.width = "73.333px";
-    elem.basicLeft = (i % cols) * s;
-    elem.style.top = (Math.floor(i / cols) * s) + 'px';
-    document.querySelector("#movingPizzas1").appendChild(elem);
-  }
-  updatePositions();
-});
+//document.addEventListener('DOMContentLoaded', function() {
+  // var cols = 8;
+  // var s = 256;
+  // for (var i = 0; i < 200; i++) {
+  //   var elem = document.createElement('img');
+  //   elem.className = 'mover';
+  //   elem.src = "images/pizza.png";
+  //   elem.style.height = "100px";
+  //   elem.style.width = "73.333px";
+  //   elem.basicLeft = (i % cols) * s;
+  //   elem.style.top = (Math.floor(i / cols) * s) + 'px';
+  //   document.querySelector("#movingPizzas1").appendChild(elem);
+  // }
+  // updatePositions();
+//});
