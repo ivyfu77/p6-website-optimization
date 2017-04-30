db04f8e45a075409eec6ad9c8964eb9620d30f67
ivyfu77 <ivy@hmsnz.com>
2017-04-30 21:40:58 +1200

    Rollback the background pizza-moving-animation

--- views/js/main.js
+++ views/js/main.js
@@ -508,19 +508,19 @@ function updatePositions() {
 // 在页面滚动时运行updatePositions函数
 window.addEventListener('scroll', updatePositions);
 
-// 取消披萨滑窗效果
-//document.addEventListener('DOMContentLoaded', function() {
-  // var cols = 8;
-  // var s = 256;
-  // for (var i = 0; i < 200; i++) {
-  //   var elem = document.createElement('img');
-  //   elem.className = 'mover';
-  //   elem.src = "images/pizza.png";
-  //   elem.style.height = "100px";
-  //   elem.style.width = "73.333px";
-  //   elem.basicLeft = (i % cols) * s;
-  //   elem.style.top = (Math.floor(i / cols) * s) + 'px';
-  //   document.querySelector("#movingPizzas1").appendChild(elem);
-  // }
-  // updatePositions();
-//});
+取消披萨滑窗效果
+document.addEventListener('DOMContentLoaded', function() {
+  var cols = 8;
+  var s = 256;
+  for (var i = 0; i < 200; i++) {
+    var elem = document.createElement('img');
+    elem.className = 'mover';
+    elem.src = "images/pizza.png";
+    elem.style.height = "100px";
+    elem.style.width = "73.333px";
+    elem.basicLeft = (i % cols) * s;
+    elem.style.top = (Math.floor(i / cols) * s) + 'px';
+    document.querySelector("#movingPizzas1").appendChild(elem);
+  }
+  updatePositions();
+});
